4ecc7f576fa60cb13d5e69c360c408a8e135e14f
Pavel <budenniy42396@gmail.com>
2019-08-13 16:34:49 +0300

    Allow armored enemy to rotate towards player (WIP)

---
+++ Assets/Scripts/RotateToFaceTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToFaceTarget : MonoBehaviour
+{
+    public float MaxAngularVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetPos =
+            GetComponent<Targeting>()
+            .Target
+            .GetComponent<Transform>()
+            .position;
+
+        var body = GetComponent<Rigidbody>();
+        var tf = GetComponent<Transform>();
+
+        var angleDiff = Vector3.SignedAngle(
+            tf.forward,
+            targetPos - tf.position,
+            Vector3.up
+            );
+
+        var angularVelocity = body.angularVelocity;
+        angularVelocity.y = Mathf.Sign(angleDiff) * Mathf.Lerp(0, MaxAngularVelocity, Mathf.Abs(angleDiff));
+        body.angularVelocity = angularVelocity;
+    }
+}
