b360a2172e4d3a3358f64e204123d6d73e565bef
Lucasazv <lukitas_azevedo@hotmail.com>
2019-08-16 09:07:39 -0300

    hello

---
+++ Assets/Scenes/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Exit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
